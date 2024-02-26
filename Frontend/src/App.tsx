7b39b5219e6805e8946e8fb3a4bee1c3b9c683a8
chandra11148 <cshekhar11148@gmail.com>
2024-02-26 14:53:25 +0530

    Uploading

---
+++ Frontend/src/App.tsx
@@ -0,0 +1,55 @@
+import { Suspense, useState } from "react";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { Landing } from "./pages/Landing";
+import { AddRecipe, Home, More, MyRecipes } from "./pages/Dashboard";
+import { DashboardLayout } from "./layouts";
+import { UILoader } from "./components/loaders";
+import { ErrorPage } from "./pages/Error";
+
+function App() {
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Landing/>,
+      errorElement: <ErrorPage/>,
+    },
+    {
+      path:"/dashboard",
+      element:<DashboardLayout/>,
+      errorElement: <ErrorPage/>,
+      children:[
+        {
+          path:'/dashboard',
+          element: <Home/>
+        },
+        {
+          path:'/dashboard/addrecipe',
+          element: <AddRecipe/>
+        },
+        {
+          path:'/dashboard/myrecipes',
+          element: <MyRecipes/>
+        },
+        {
+          path:'/dashboard/recipe/:id',
+          element: <More/>
+        },
+        
+      ]
+    }
+  ]);
+
+  return (
+    
+      <div className="h-[100vh] w-[100vw]">
+        <Suspense fallback={<UILoader/>}>
+          <RouterProvider
+            router={router}
+            fallbackElement={<UILoader/>}
+          />
+        </Suspense>
+      </div>
+  );
+}
+
+export default App;
