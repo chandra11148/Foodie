424faaf67c102d9efa69164a36b98b3f3e2f9910
chandra11148 <cshekhar11148@gmail.com>
2024-02-26 20:29:25 +0530

    Backend

--- Backend/src/index.ts
+++ Backend/src/index.ts
@@ -12,12 +12,8 @@ import fileUpload from "express-fileupload";
 
 
 const app:Application = express();
-app.use(cors({
-    origin: `https://foodie-ivory-nu.vercel.app/`,
-    allowedHeaders: "Origin, X-Requested-With, Content-Type, Accept",
-    credentials: true,
-  })
- );
+app.use(cors(
+ ));
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 
